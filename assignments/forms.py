edafd56a6f71608c4f3131bb0ddc1e595ba74e56
JosueDLA <josuedlavs@gmail.com>
2022-06-05 22:34:59 -0600

    Update Assigments Form

--- assignments/forms.py
+++ assignments/forms.py
@@ -6,11 +6,13 @@ from django.utils import timezone
 from courses.models import Course
 from users.models import User
 
+
 class GradeAssignmentForm(ModelForm):
-    
+
     class Meta:
         model = SubmitAssignment
         fields = ['grade']
+
 
 class CreateAssignmentForm(ModelForm):
     class Meta:
@@ -18,7 +20,10 @@ class CreateAssignmentForm(ModelForm):
         fields = ('assignment_name', 'assignment_description',
                   'due_date', 'course')
         labels = {
-            'due_date': 'Due Date (yyyy-mm-dd HH:MM)'
+            'assignment_name': 'Nombre de la Tarea',
+            'assignment_description': 'Descripción',
+            'due_date': 'Fecha de Entrega (yyyy-mm-dd HH:MM)',
+            'course': 'Curso'
         }
 
     def __init__(self, *args, **kwargs):
@@ -26,16 +31,21 @@ class CreateAssignmentForm(ModelForm):
         super().__init__(*args, **kwargs)
         user_object = User.objects.filter(username=user.username)
         new_user_object = get_object_or_404(user_object)
-        self.fields['course'].queryset = self.fields['course'].queryset.filter(teacher=new_user_object.id)
+        self.fields['course'].queryset = self.fields['course'].queryset.filter(
+            teacher=new_user_object.id)
+
 
 class SubmitAssignmentForm(ModelForm):
     class Meta:
         model = SubmitAssignment
-        fields = ('topic', 'description', 'assignment_file', 'assignment_ques', 'author')
-        
+        fields = ('topic', 'description', 'assignment_file',
+                  'assignment_ques', 'author')
+
     def __init__(self, *args, **kwargs):
         user = kwargs.pop('user')
         assignment = kwargs.pop('assignment_id')
         super().__init__(*args, **kwargs)
-        self.fields['assignment_ques'].queryset = self.fields['assignment_ques'].queryset.filter(pk=assignment)
-        self.fields['author'].queryset = self.fields['author'].queryset.filter(username=user.username)
+        self.fields['assignment_ques'].queryset = self.fields['assignment_ques'].queryset.filter(
+            pk=assignment)
+        self.fields['author'].queryset = self.fields['author'].queryset.filter(
+            username=user.username)
